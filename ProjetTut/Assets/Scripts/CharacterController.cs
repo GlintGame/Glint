ea6d9dfee6fede82b8ce8059582d1b2c50795eee
theo lemoine <theo.lemoine0@gmail.com>
2018-09-10 19:45:56 +0200

    Added cammera follow, changed the jump to make it more reactive

--- ProjetTut/Assets/Scripts/CharacterController.cs
+++ ProjetTut/Assets/Scripts/CharacterController.cs
@@ -8,7 +8,7 @@ public class CharacterController : MonoBehaviour
     [Range(.2f, 1f)] public float WalkSpeedModifier = .8f;
     [Range(.2f, 1f)] public float InAirSpeedModifier = .6f;
     public Rigidbody2D RigidBody;
-    [Range(0, 2000f)] public float JumpForce = 700f;
+    [Range(0, 40f)] public float JumpForce = 20f;
     public LayerMask GroundLayer;
     public Transform GroundCheck;
 
@@ -16,7 +16,7 @@ public class CharacterController : MonoBehaviour
 
     private Vector3 _velocity = Vector3.zero;
     private bool _grounded = true;
-    const float GroundedRadius = 0.5f;
+    const float GroundedRadius = 1f;
 
     public void Move(float xspeed, bool jump, bool running)
     {
@@ -31,14 +31,14 @@ public class CharacterController : MonoBehaviour
             xspeed *= this.WalkSpeedModifier;
         }
 
-        Vector3 targetVelocity = new Vector2(xspeed, RigidBody.velocity.y);
+        Vector3 targetVelocity = new Vector2(xspeed, this.RigidBody.velocity.y);
         this.RigidBody.velocity = Vector3.SmoothDamp(this.RigidBody.velocity, targetVelocity, ref this._velocity, this.MovementSmothing);
 
 
         if (jump && this._grounded)
         {
-            var jumpVector = new Vector2(0.0f, this.JumpForce);
-            this.RigidBody.AddForce(jumpVector);
+            var jumpVector = new Vector2(this.RigidBody.velocity.x , this.JumpForce);
+            this.RigidBody.velocity = jumpVector;
 
             this._grounded = false;
         }
