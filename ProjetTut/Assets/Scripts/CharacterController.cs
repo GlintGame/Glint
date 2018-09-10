a07683ae2e8c91b7afee5c1c3de7bd9ffabc7187
theo lemoine <theo.lemoine0@gmail.com>
2018-09-10 16:52:43 +0200

    1 : Added a CharacterController script to handle moving any character in space, also added a PlayerInputController, to call other components depending on the player inputs. created a scene using it.

---
+++ ProjetTut/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float WalkSpeedModifier = .8f;
+    public Rigidbody2D RigidBody;
+    public Collider2D Collider;
+    public float JumpForce = 400f;
+    public LayerMask GroundLayer;
+    public Transform GroundCheck;
+
+    [Range(0, .3f)] public float MovementSmothing = .05f;
+
+    private Vector3 _velocity = Vector3.zero;
+    private bool _grounded = true;
+    const float GroundedRadius = 0.2f;
+
+    public void Move(float xspeed, bool jump, bool running)
+    {
+        // set the horizontal speed
+        xspeed = running? xspeed : xspeed * this.WalkSpeedModifier ;
+        Vector3 targetVelocity = new Vector2(xspeed, RigidBody.velocity.y);
+        this.RigidBody.velocity = Vector3.SmoothDamp(this.RigidBody.velocity, targetVelocity, ref this._velocity, this.MovementSmothing);
+
+
+        if (jump && this._grounded)
+        {
+            var jumpVector = new Vector2(0.0f, this.JumpForce);
+            this.RigidBody.AddForce(jumpVector);
+
+            this._grounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, GroundedRadius, GroundLayer);
+
+        bool IsGrounded = false;
+        foreach ( Collider2D collider in colliders)
+        {
+            if (collider.gameObject != this.gameObject)
+            {
+                IsGrounded = true;
+            }
+        }
+
+        this._grounded = IsGrounded;
+
+        Debug.Log(this._grounded);
+    }
+    
+}
