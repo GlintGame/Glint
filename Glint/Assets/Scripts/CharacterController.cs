d26531d263f30fe226cdcf613d6281bc69468f6b
theo lemoine <theo.lemoine0@gmail.com>
2018-09-11 13:53:37 +0200

    Changed file name to Glint

---
+++ Glint/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [Range(.2f, 1f)] public float WalkSpeedModifier = .8f;
+    [Range(.2f, 1f)] public float InAirSpeedModifier = .6f;
+    public Rigidbody2D RigidBody;
+    [Range(0, 40f)] public float JumpForce = 20f;
+    public LayerMask GroundLayer;
+    public Transform GroundCheck;
+
+    [Range(0, .3f)] public float MovementSmothing = .05f;
+
+    private Vector3 _velocity = Vector3.zero;
+    private bool _grounded = true;
+    const float GroundedRadius = 1f;
+
+    public void Move(float xspeed, bool jump, bool running)
+    {
+
+        // set the horizontal speed
+        if (!this._grounded)
+        {
+            xspeed *= this.InAirSpeedModifier;
+        }
+        else if(!running)
+        {
+            xspeed *= this.WalkSpeedModifier;
+        }
+
+        Vector3 targetVelocity = new Vector2(xspeed, this.RigidBody.velocity.y);
+        this.RigidBody.velocity = Vector3.SmoothDamp(this.RigidBody.velocity, targetVelocity, ref this._velocity, this.MovementSmothing);
+
+
+        if (jump && this._grounded)
+        {
+            var jumpVector = new Vector2(this.RigidBody.velocity.x , this.JumpForce);
+            this.RigidBody.velocity = jumpVector;
+
+            this._grounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, GroundedRadius, GroundLayer);
+
+        bool IsGrounded = false;
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != this.gameObject)
+            {
+                IsGrounded = true;
+            }
+        }
+
+        this._grounded = IsGrounded;        
+    }
+}
---
+++ Glint/Assets/Scripts/PlayerInputsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerInputsController : MonoBehaviour
+{
+
+    public CharacterController Controller;
+    public float SpeedMultiplier = 30;
+
+    private bool _jumping;
+    private float _movementSpeed;
+
+
+	void Update ()
+	{
+	    this._jumping = Input.GetButtonDown("Jump");
+	    this._movementSpeed = Input.GetAxisRaw("Horizontal") * this.SpeedMultiplier;
+	}
+
+    void FixedUpdate()
+    {
+        Controller.Move(_movementSpeed, _jumping, true);
+    }
+}
