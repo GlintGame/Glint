d016e043bd073854cb73730810e33924f4deb75b
theo lemoine <theo.lemoine0@gmail.com>
2018-09-13 21:38:25 +0200

    finished the fix on jump animation, added inertia to air movements and set the camera to not lookahead when jumping because of aceleration

--- Glint/Assets/Scripts/CharacterController.cs
+++ Glint/Assets/Scripts/CharacterController.cs
@@ -7,32 +7,41 @@ using UnityEngine.Events;
 public class CharacterController : MonoBehaviour
 {
     [Range(.2f, 1f)] public float WalkSpeedModifier = .8f;
-    [Range(.2f, 1f)] public float InAirSpeedModifier = .6f;
     [Range(0, .3f)] public float MovementSmothing = .05f;
-    [Range(0, 60f)] public float JumpForce = 20f;
+    [Range(0, 60f)] public float JumpForce = 35f;
     public Rigidbody2D RigidBody;
     public LayerMask GroundLayer;
+    public float AirVelocityToForceFactor = 50;
 
     public UnityEvent OnLanding;
     
     private Vector3 _velocity = Vector3.zero;
     private bool _grounded = true;
     private bool _facingRight = true;
-    private const float GroundedRadius = 1f;
-    private ContactPoint2D[] _colisionContacts;
+    private bool _jumpingRight = false;
+    
 
     public void Move(float xspeed, bool jump, bool running)
     {
 
-        // set the horizontal speed
-        if (!this._grounded)
+        Vector3 targetVelocity = new Vector2(xspeed, this.RigidBody.velocity.y);
+        if (!running)
         {
-            xspeed *= this.InAirSpeedModifier;
+            targetVelocity.x *= this.WalkSpeedModifier;
         }
-        else if(!running)
+
+        if (this._grounded)
         {
-            xspeed *= this.WalkSpeedModifier;
+            this.RigidBody.velocity = Vector3.SmoothDamp(this.RigidBody.velocity, targetVelocity, ref this._velocity, this.MovementSmothing);
+
         }
+        else if ((xspeed > 0 && !this._jumpingRight)
+                || (xspeed < 0 && this._jumpingRight)) 
+        {          
+            // in air there is the body inertia, so we cant just overide the old velocity
+            targetVelocity.x *= this.AirVelocityToForceFactor;
+            this.RigidBody.AddForce(targetVelocity);
+        }        
 
         if (xspeed < 0 && _facingRight)
         {
@@ -43,9 +52,7 @@ public class CharacterController : MonoBehaviour
             this.FlipSprite();
         }
 
-        Vector3 targetVelocity = new Vector2(xspeed, this.RigidBody.velocity.y);
-        this.RigidBody.velocity = Vector3.SmoothDamp(this.RigidBody.velocity, targetVelocity, ref this._velocity, this.MovementSmothing);
-
+        
         // jump mechanic
         if (jump && this._grounded)
         {
@@ -53,18 +60,9 @@ public class CharacterController : MonoBehaviour
             this.RigidBody.velocity = jumpVector;
 
             this._grounded = false;
-        }
-    }
-
-    private void Awake()
-    {
-        this._colisionContacts = new ContactPoint2D[200];
-    }
-
-    private void Update()
-    {
-        Debug.Log(this._grounded);
-    }
+            this._jumpingRight = this._facingRight;
+        }        
+    }     
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -77,13 +75,23 @@ public class CharacterController : MonoBehaviour
     }
 
     private void OnCollisionExit2D(Collision2D collision)
-    {
-        
-        collision.GetContacts(this._colisionContacts);
+    {        
 
         if (collision.collider.gameObject.layer == (int) Mathf.Log(this.GroundLayer.value, 2))
         {
             this._grounded = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!this._grounded)
+        {
+            if (collision.collider.gameObject.layer == (int)Mathf.Log(this.GroundLayer.value, 2)
+            && collision.relativeVelocity.y > 0)
+            {
+                this._grounded = true;
+            }
         }
     }
 
--- Glint/Assets/Scripts/PlayerInputsController.cs
+++ Glint/Assets/Scripts/PlayerInputsController.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerInputsController : MonoBehaviour
 {
     public CharacterController Controller;
     public float SpeedMultiplier = 30;
     public Animator PlayerAnimator;
+    public CinemachineVirtualCamera CVCamera;
 
     private bool _jumping;
     private float _movementSpeed;
     private bool _running;
+    private float _lookaheadTime;
+    private CinemachineFramingTransposer body;
 
+    void Awake()
+    {
+        // the body is the first array element cause it's the first in the unity editor...
+        // and fuck thoses class names...
+        this.body = this.CVCamera.GetComponentPipeline()[0] as CinemachineFramingTransposer;
+        this._lookaheadTime = body.m_LookaheadTime;
+    }
 
-    void Update()
+    void FixedUpdate()
     {
         this._jumping = Input.GetButtonDown("Jump");
         this._movementSpeed = Input.GetAxisRaw("Horizontal") * this.SpeedMultiplier;
@@ -23,20 +33,30 @@ public class PlayerInputsController : MonoBehaviour
         {
             this._jumping = true;
             this.PlayerAnimator.SetBool("PlayerJump", true);
+            this.body.m_LookaheadTime = 0;
         }
 
         this._running = !(Input.GetButton("Fire3"));
         this.PlayerAnimator.SetBool("PlayerRunning", this._running);
-        this.PlayerAnimator.SetFloat("PlayerMovement", Mathf.Abs(this._movementSpeed));        
-    }
+        this.PlayerAnimator.SetFloat("PlayerMovement", Mathf.Abs(this._movementSpeed));
 
-    void FixedUpdate()
-    {
         Controller.Move(this._movementSpeed, this._jumping, this._running);
     }
 
     public void OnLanding()
     {
         this.PlayerAnimator.SetBool("PlayerJump", false);
+        StartCoroutine("DoLerp");
+    }
+
+    IEnumerator DoLerp()
+    {
+        Debug.Log("lerping");
+        while (this.body.m_LookaheadTime < this._lookaheadTime)
+        {
+            Debug.Log("lerping");
+            this.body.m_LookaheadTime = Mathf.Lerp(this.body.m_LookaheadTime, this._lookaheadTime, 0.1f);
+            yield return null;
+        }        
     }
 }
