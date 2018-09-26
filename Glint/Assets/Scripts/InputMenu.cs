fd3010e22b737aebd1ff55015542932316016b27
Léon Baudouin <leondedouin@gmail.com>
2018-09-26 20:09:47 +0200

    4.5 Added a SetProperty function to change an input based on a string

---
+++ Glint/Assets/Scripts/InputMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMenu : MonoBehaviour {
+
+    Event keyEvent;
+    private bool _waitingForKey = false;
+    private InputManagerManager _inputManagerManager;
+    private string _axisName;
+    private string _propertyName;
+
+    void Awake()
+    {
+        this._inputManagerManager = gameObject.GetComponent<InputManagerManager>();
+        genDictionnary();
+    }
+
+    void OnGUI()
+    {
+        Dictionary<KeyCode, string> convertKeyToStr;
+        keyEvent = Event.current;
+        if(keyEvent.isKey /*&& _waitingForKey*/)
+        {
+            _waitingForKey = false;
+            Debug.Log(keyEvent.keyCode);
+            //InputManagerManager.SetProperty(_axisName, _propertyName, keyEvent.keyCode);
+        }
+    }
+
+    private Dictionary<string, string> genDictionnary()
+    {
+        Dictionary<string, string> dictionnary = new Dictionary<string, string>();
+        string[] alphabet = new string[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        for(int i = 0; i < 26; i++)
+        {
+            dictionnary.Add(alphabet[i], alphabet[i]);
+        }
+        Debug.Log(dictionnary);
+        return dictionnary;
+    }
+}
