fe96db94e73c3dc5d7c690d0170372a4be769887
Léon Baudouin <leondedouin@gmail.com>
2019-02-02 21:16:07 +0100

    1.4 Fixed some minor stuff nothing much xoxo

---
+++ Glint/Assets/DisplayOnTop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DisplayOnTop : MonoBehaviour {
+
+	void OnSceneLoaded()
+    {
+        this.transform.SetAsLastSibling();
+    }
+}
--- Glint/Assets/TutoTrigger.cs
+++ Glint/Assets/TutoTrigger.cs
@@ -11,33 +11,55 @@ public class TutoTrigger : MonoBehaviour {
     public BindingButton keyboardParams;
     public BindingButton gamepadParams;
 
+    [Range(0f, 5.0f)]
+    public float displayDuration = 1.0f;
+
     private TextMeshProUGUI text;
 
-    private float _currentTextDuration;
-    private float _overallTextDuration;
 
     void Awake()
     {
         this.text = this.tutoCanvas.GetComponentInChildren<TextMeshProUGUI>();
-        this.text.text = this.text.text.Replace("${KB}", InputsDictionnary.getSpriteIndex(keyboardParams));
-        this.text.text = this.text.text.Replace("${GP}", InputsDictionnary.getSpriteIndex(gamepadParams));
+
+        if (this.keyboardParams)
+                this.text.text = this.text.text.Replace("${KB}", InputsDictionnary.getSpriteIndex(this.keyboardParams));
+
+        if (this.gamepadParams)
+            this.text.text = this.text.text.Replace("${GP}", InputsDictionnary.getSpriteIndex(this.gamepadParams));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        StartCoroutine(ShowText());
-        this.tutoCanvas.gameObject.SetActive(true);
+        StartCoroutine(this.Wait());
+        this.ShowText();
     }
 
-    private IEnumerator ShowText()
-    {
-        yield return new WaitWhile(
-            () => !(InputManager.GetButton(this.keyboardParams.action)
-                || InputManager.GetAxis(this.keyboardParams.action) > 0.9)
-        );
-        yield return new WaitForSeconds(1.0f);
 
+    private IEnumerator Wait()
+    {
+        if (this.keyboardParams || this.gamepadParams)
+        {
+            yield return new WaitWhile(
+                () => !(InputManager.GetButton(this.keyboardParams.action)
+                    || InputManager.GetAxis(this.keyboardParams.action) > 0.9)
+            );
+        }
+
+        yield return new WaitForSeconds(displayDuration);
+
+
+        this.HideText();
+    }
+
+    private void ShowText()
+    {
+        this.tutoCanvas.gameObject.SetActive(true);
+    }
+    
+    private void HideText()
+    {
         this.tutoCanvas.gameObject.SetActive(false);
         Destroy(this.gameObject);
     }
+
 }
