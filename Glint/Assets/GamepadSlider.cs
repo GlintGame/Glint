b44f1f5fecf438a89021b2e75a7e10c4be194d30
LeonBaudouin <leondedouin@gmail.com>
2019-02-10 19:57:24 +0100

    1.5 : Started script to manage slider with gamepad. 1.G : Added disable and clicked sprite for button

---
+++ Glint/Assets/GamepadSlider.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Luminosity.IO;
+
+public class GamepadSlider : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+
+    private bool focused = false;
+    private bool isSelected = false;
+    //private Slider slider;
+    private EventSystem eventSystem;
+    public GameObject eventSystemGameObject;
+
+    void Awake()
+    {
+        //this.slider = this.gameObject.GetComponent<Slider>();
+        this.eventSystem = this.eventSystemGameObject.GetComponent<EventSystem>();
+    }
+
+    void Update () {
+        if(this.focused
+            && !this.isSelected
+            && InputManager.GetButton("UI_Submit"))
+        {
+            Debug.Log(this.eventSystem);
+            this.isSelected = true;
+            this.eventSystemGameObject.SetActive(false);
+        }
+
+        if(this.isSelected
+            && InputManager.GetButton("UI_Submit"))
+        {
+            this.isSelected = false;
+            this.eventSystemGameObject.SetActive(true);
+            this.eventSystem.SetSelectedGameObject(this.gameObject);
+        }
+	}
+
+    public void OnSelect(BaseEventData baseEventData)
+    {
+        this.focused = true;
+    }
+
+    public void OnDeselect(BaseEventData baseEventData)
+    {
+        this.focused = false;
+    }
+}
