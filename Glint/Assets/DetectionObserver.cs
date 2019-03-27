88a094ce05c1690495d5f2df92d2d95956fd5a22
théo lemoine <theo.lemoine0@gmail.com>
2019-03-28 00:27:21 +0100

    Refactored Slashy and added a first eggsy prefab

--- Glint/Assets/DetectionObserver.cs
+++ Glint/Assets/DetectionObserver.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DetectionObserver : MonoBehaviour
 {
-    public SlashyAI AI;
+    public int detectionLayer = 9;
+
+    [System.Serializable]
+    public class DetectionEvent : UnityEvent<GameObject> { }
+
+    public DetectionEvent OnDetectOut;
+    public DetectionEvent OnDetectIn;
+    public DetectionEvent OnDetectStay;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == this.detectionLayer)
+            this.OnDetectOut.Invoke(collision.gameObject);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 9)
-            this.AI.OnDetected(collision.gameObject);
+        if (collision.gameObject.layer == this.detectionLayer)
+            this.OnDetectIn.Invoke(collision.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == this.detectionLayer)
+            this.OnDetectStay.Invoke(collision.gameObject);
     }
 }
---
+++ Glint/Assets/EggsyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using utils;
+
+public class EggsyAI : MonoBehaviour
+{
+    private BoxCollider2D Collider;
+
+    private void Awake()
+    {
+        this.Collider = this.GetComponent<BoxCollider2D>();
+        this.Collider.enabled = false;
+    }
+
+    public void OnDetected()
+    {
+        this.Collider.enabled = true;
+
+        StartCoroutine(utils.Coroutine.Do.Wait(3f, () => this.Collider.enabled = false));
+    }
+
+    public void OffDetected()
+    {
+        this.Collider.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // make dmg
+    }
+}
